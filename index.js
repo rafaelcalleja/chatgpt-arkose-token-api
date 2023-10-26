adb657f8150296cd8bc8d1bb82d032d990a49233
Rafael Calleja <rafaelcalleja@gmail.com>
2023-10-26 14:06:01 +0200

    key dynamic

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // noinspection SpellCheckingInspection
 
 const http = require('http');
+const url = require('url');
 const fun = require('funcaptcha');
 
 http.createServer(async (req, res) => {
   console.log('...')
+  const requestUrl = req.url;
+
+  const parsedUrl = url.parse(requestUrl, true);
+  const query = parsedUrl.query;
+
+  const key = query.key || "35536E1E-65B4-4D96-9D97-6ADB7EFF8147";
   const token = await fun.getToken({
-    pkey: "35536E1E-65B4-4D96-9D97-6ADB7EFF8147",
+    pkey: key,
     surl: "https://tcr9i.chat.openai.com",
     headers: {
       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
